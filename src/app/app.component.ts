90b525fb38572bfd2149a22f5b425a47f76005b0
LordPrinz <plazinski2902@gmail.com>
2022-11-17 15:16:10 +0100

    Create redirection buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ball';
+
+  constructor(private router: Router) {}
+
+  createPlayerRedirectHandler() {
+    this.router.navigate(['/players/new']);
+  }
+  createTeamRedirectHandler() {
+    this.router.navigate(['/teams/new']);
+  }
 }
