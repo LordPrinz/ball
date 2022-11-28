eef49c2de78d62086fcde30d461c27c15baa8ce2
Android <as@informa.zsti>
2022-11-28 09:14:15 +0100

    Create player team operation

--- src/app/team-generator/team-generator.component.ts
+++ src/app/team-generator/team-generator.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { player } from 'src/types/player';
+
+type playerDialog = {
+  player: player;
+  isSelected: boolean;
+};
 
 @Component({
   selector: 'app-team-generator',
@@ -9,8 +15,36 @@ export class TeamGeneratorComponent implements OnInit {
   ngOnInit(): void {}
 
   name: string = '';
+  selectedPlayers: playerDialog[] = [];
+
+  addPlayerToTeam(playerId: string) {
+    const player = this.selectedPlayers.find(
+      (player) => player.player._id === playerId
+    )!;
+    this.selectedPlayers = this.selectedPlayers.filter(
+      (player) => player.player._id !== playerId
+    );
+
+    player.isSelected = true;
+    this.selectedPlayers.push(player);
+  }
+
+  removePlayerFromTeam(playerId: string) {
+    const player = this.selectedPlayers.find(
+      (player) => player.player._id === playerId
+    )!;
+    this.selectedPlayers = this.selectedPlayers.filter(
+      (player) => player.player._id !== playerId
+    );
+    player.isSelected = false;
+    this.selectedPlayers.push(player);
+  }
 
   nameInputHandler(event: any) {
     this.name = event.target.value;
+  }
+
+  submitHandler() {
+    console.log('XD');
   }
 }
