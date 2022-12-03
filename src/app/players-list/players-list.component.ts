e0f8bc79d3973cd474c806e2f76892aefd1457e2
LordPrinz <plazinski2902@gmail.com>
2022-12-03 17:06:42 +0100

    Create player edit component

--- src/app/players-list/players-list.component.ts
+++ src/app/players-list/players-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { player } from 'src/types/player';
 import { HttpService } from '../config/http.service';
+import { PlayerEditComponent } from '../player-edit/player-edit.component';
 
 @Component({
   selector: 'app-players-list',
@@ -11,11 +13,25 @@ export class PlayersListComponent implements OnInit {
   allPlayers: player[] | undefined;
   hovering: string | null = null;
 
-  constructor(private http: HttpService) {}
+  constructor(private http: HttpService, public dialog: MatDialog) {}
 
-  deleteHandler(id: any): void {
+  deleteHandler(id: string): void {
     this.allPlayers = this.allPlayers?.filter((p) => p._id !== id);
     this.http.deletePlayer(id).subscribe((response) => {});
+  }
+
+  editHandler(id: string) {
+    const popup = this.dialog.open(PlayerEditComponent, {
+      data: this.allPlayers?.find((player) => player._id === id),
+    });
+
+    popup.afterClosed().subscribe((data) => {
+      if (!data) {
+        return;
+      }
+
+      console.log(data);
+    });
   }
 
   ngOnInit(): void {
