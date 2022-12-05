d94c65227183e9da509006634a3e705165c68a63
Android <as@informa.zsti>
2022-12-05 09:33:21 +0100

    Create players filter

--- src/app/edit-team-dialog/edit-team-dialog.component.ts
+++ src/app/edit-team-dialog/edit-team-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { player } from 'src/types/player';
+import { HttpService } from '../config/http.service';
 
 @Component({
   selector: 'app-edit-team-dialog',
@@ -10,10 +12,21 @@ export class EditTeamDialogComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<EditTeamDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private Ref: MatDialogRef<EditTeamDialogComponent>
+    private Ref: MatDialogRef<EditTeamDialogComponent>,
+    private http: HttpService
   ) {}
 
+  selectedPlayers: player[] = [];
+  notSelectedPlayers: player[] = [];
+  allPlayers: player[] | null = [];
+
   ngOnInit(): void {
-    console.log(this.data);
+    this.selectedPlayers = this.data.players;
+    this.http.getPlayers().subscribe((data) => {
+      this.allPlayers = (data as any).data;
+      this.notSelectedPlayers = this.allPlayers?.filter(
+        (ar) => !this.selectedPlayers.find((rm) => ar._id === rm._id)
+      )!;
+    });
   }
 }
